0a1ddb803a7f55b513970d876abe5cd4c513a2cc
KhusniddinovUz <khusniddinovuz@gmail.com>
2020-11-25 21:36:46 +0500

    Create view and url

--- password_generator-project/generator/views.py
+++ password_generator-project/generator/views.py
@@ -1,3 +1,12 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def home(request):
+    return HttpResponse('Hello there friend')
+
+
+def eggs(request):
+    return HttpResponse('Eggs are so tasty')
