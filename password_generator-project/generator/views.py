c720f72bde65e45d960ee077977512c2a28c94c6
KhusniddinovUz <khusniddinovuz@gmail.com>
2020-11-29 00:31:43 +0500

    Get data from FORM and make random password

--- password_generator-project/generator/views.py
+++ password_generator-project/generator/views.py
@@ -1,12 +1,30 @@
 from django.shortcuts import render
-
+import random
 
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'generator/home.html', {'password': 'd19fhewahf8'})
+    return render(request, 'generator/home.html')
 
 
 def password(request):
-    return render(request, 'generator/password.html', {'password': 'd19fhewahf8'})
+    characters = list('abcdefghijklomnpqrstuvwxyz')
+
+    if request.GET.get('uppercase'):
+        characters.extend(list('ABCDEFGHIJKLMONPQRSTUVWXYZ'))
+
+    if request.GET.get('special'):
+        characters.extend(list('!@#$%^&*()'))
+
+    if request.GET.get('numbers'):
+        characters.extend(list('0123456789'))
+
+    length = int(request.GET.get('length', 10))
+
+    thepass = ''
+
+    for x in range(length):
+        thepass += random.choice(characters)
+
+    return render(request, 'generator/password.html', {'password': thepass})
