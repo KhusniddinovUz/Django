3f281724b26999b3a4d033531851aa3f4bbb8289
KhusniddinovUz <khusniddinovuz@gmail.com>
2021-02-15 18:48:28 +0500

    add url for sharing post

--- blogproject/blog/views.py
+++ blogproject/blog/views.py
@@ -25,7 +25,7 @@ def post_share(request, post_id):
         form = EmailPostForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
-            post_url = request.build_absolute_uri(
+            post_url = request.build_absolute_url(
                 post.get_absolute_url())
             subject = f"{cd['name']} recommends you read {post.title}"
             message = f"Read {post.title} at {post_url}\n\n" \
